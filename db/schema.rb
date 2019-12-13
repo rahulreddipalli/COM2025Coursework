d2f4a8ea87378358ae0cea8a2fe46f055c407ec7
Reddipalli <rr00486@duck10.eps.surrey.ac.uk>
2019-12-13 22:04:40 +0000

    authentication implemented

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_13_201706) do
+ActiveRecord::Schema.define(version: 2019_12_13_214425) do
 
   create_table "games", force: :cascade do |t|
     t.string "name"
