f37f0ce36f40c16068a1ebca73ffdbf922c875eb
Reddipalli <rr00486@heron093.eps.surrey.ac.uk>
2019-12-15 06:57:21 +0000

    beginning testing

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -25,6 +25,6 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     post request_contact_url, params:{name: "Matthew", email: "matthew@me.com", telephone: "1234567890", message: "Hello"}
     assert_response :redirect
     assert_nil flash[:alert]
-    assert_not_empty flash[:notice
+    assert_not_empty flash[:notice]
     end
 end
--- test/mailers/contact_mailer_test.rb
+++ test/mailers/contact_mailer_test.rb
@@ -4,11 +4,12 @@ class ContactMailerTest < ActionMailer::TestCase
   # test "the truth" do
   #   assert true
   # end
-  test "should return contact email" do
-    mail = ContactMailer.contact_email("matthew@me.com","Matthew Casey", "1234567890", @message = "Hello")
+    test "should return contact email" do
+      mail = ContactMailer.contact_email("matthew@me.com",
+        "Matthew Casey", "1234567890", @message = "Hello")
 
-    assert_equal['info@mynotes.com'], mail.to
-    assert_equal['info@mynotes.com'], mail.from
-  end
+      assert_equal ['info@mynotes.com'], mail.to
+      assert_equal ['info@mynotes.com'], mail.from
+    end
 
 end
